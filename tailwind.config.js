863e43f61e6572b818457cd7c4b5ce52ec03634b
Kasope Johnson <kasopej@gmail.com>
2022-09-25 00:57:51 +0100

    adds responsive column styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        "lg-t":
+          "var(--tw-ring-offset-shadow, 10px 0 #0000), var(--tw-ring-shadow, 10px 0 #0000), var(--tw-shadow);",
+      },
+    },
   },
   plugins: [require("@tailwindcss/typography")],
 };
