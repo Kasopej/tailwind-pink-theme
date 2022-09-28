8771d77967771177ece522bbbf0206ffed0e5218
Kasope Johnson <kasopej@gmail.com>
2022-09-28 05:17:26 +0100

    sets styling for responsiveness

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
+      screens: {
+        lg: "992px",
+      },
       boxShadow: {
         "lg-t":
           "var(--tw-ring-offset-shadow, 10px 0 #0000), var(--tw-ring-shadow, 10px 0 #0000), var(--tw-shadow);",
