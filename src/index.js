a643c26d0f3208d3106bb9bab60774a218e14b4d
Kasope Johnson <kasopej@gmail.com>
2022-10-01 05:25:17 +0100

    adds mobile nav styling

--- src/index.js
+++ src/index.js
@@ -5,8 +5,23 @@ function transitionImageIntoView() {
   this.classList.add("inview");
 }
 
+function openMobileMenu() {
+  this.classList.toggle("mobile-menu-open");
+}
+
+function toggleNavMobileStyle() {
+  this.classList.toggle("nav-mobile-style");
+}
+
 document.querySelectorAll(".image-copy").forEach((el) => {
   el.addEventListener("mouseenter", function () {
     transitionImageIntoView.call(this);
   });
+});
+
+document.querySelector(".hamburger-icon").addEventListener("click", () => {
+  const mobileMenu = document.querySelector(".mobile-menu");
+  const nav = document.getElementsByTagName("nav")[0];
+  openMobileMenu.call(mobileMenu);
+  toggleNavMobileStyle.call(nav);
 });
