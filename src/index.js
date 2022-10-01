cb688a7a487b1b71c974ff1f7858aad0368e010f
Kasope Johnson <kasopej@gmail.com>
2022-10-01 04:07:24 +0100

    adds transition effect via dynamically added css class

--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
 import "../assets/style/css/main.css";
 import "../assets/style/sass/main.scss";
+
+function transitionImageIntoView() {
+  this.classList.add("inview");
+}
+
+document.querySelectorAll(".image-copy").forEach((el) => {
+  el.addEventListener("mouseenter", function () {
+    transitionImageIntoView.call(el);
+  });
+});
